a905e72b383b655f1e512254eb8fbae832fe3f84
Erlend Dalen <erlend.dalen@gmail.com>
2017-12-05 10:43:14 +0100

    Added apps and use django.migration
    Deleted old south migrations.

--- cmsplugin_poll/migrations/0001_initial.py
+++ cmsplugin_poll/migrations/0001_initial.py
@@ -1,89 +1,61 @@
-# encoding: utf-8
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-class Migration(SchemaMigration):
-
-    def forwards(self, orm):
-        
-        # Adding model 'Poll'
-        db.create_table('cmsplugin_poll_poll', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('question', self.gf('django.db.models.fields.CharField')(max_length=300)),
-            ('pub_date', self.gf('django.db.models.fields.DateTimeField')()),
-        ))
-        db.send_create_signal('cmsplugin_poll', ['Poll'])
-
-        # Adding model 'Choice'
-        db.create_table('cmsplugin_poll_choice', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('poll', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['cmsplugin_poll.Poll'])),
-            ('choice', self.gf('django.db.models.fields.CharField')(max_length=200)),
-            ('votes', self.gf('django.db.models.fields.IntegerField')(default=0)),
-        ))
-        db.send_create_signal('cmsplugin_poll', ['Choice'])
-
-        # Adding model 'PollPlugin'
-        db.create_table('cmsplugin_pollplugin', (
-            ('cmsplugin_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['cms.CMSPlugin'], unique=True, primary_key=True)),
-            ('poll', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['cmsplugin_poll.Poll'])),
-        ))
-        db.send_create_signal('cmsplugin_poll', ['PollPlugin'])
+from django.db import models, migrations
 
 
-    def backwards(self, orm):
-        
-        # Deleting model 'Poll'
-        db.delete_table('cmsplugin_poll_poll')
+class Migration(migrations.Migration):
 
-        # Deleting model 'Choice'
-        db.delete_table('cmsplugin_poll_choice')
+    dependencies = [
+        ('cms', '0013_auto_20160404_1908'),
+    ]
 
-        # Deleting model 'PollPlugin'
-        db.delete_table('cmsplugin_pollplugin')
-
-
-    models = {
-        'cms.cmsplugin': {
-            'Meta': {'object_name': 'CMSPlugin'},
-            'creation_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'language': ('django.db.models.fields.CharField', [], {'max_length': '15', 'db_index': 'True'}),
-            'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.CMSPlugin']", 'null': 'True', 'blank': 'True'}),
-            'placeholder': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Placeholder']", 'null': 'True'}),
-            'plugin_type': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'}),
-            'position': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
-        },
-        'cms.placeholder': {
-            'Meta': {'object_name': 'Placeholder'},
-            'default_width': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'slot': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'})
-        },
-        'cmsplugin_poll.choice': {
-            'Meta': {'object_name': 'Choice'},
-            'choice': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'poll': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cmsplugin_poll.Poll']"}),
-            'votes': ('django.db.models.fields.IntegerField', [], {'default': '0'})
-        },
-        'cmsplugin_poll.poll': {
-            'Meta': {'object_name': 'Poll'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'pub_date': ('django.db.models.fields.DateTimeField', [], {}),
-            'question': ('django.db.models.fields.CharField', [], {'max_length': '300'})
-        },
-        'cmsplugin_poll.pollplugin': {
-            'Meta': {'object_name': 'PollPlugin', 'db_table': "'cmsplugin_pollplugin'", '_ormbases': ['cms.CMSPlugin']},
-            'cmsplugin_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'}),
-            'poll': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cmsplugin_poll.Poll']"})
-        }
-    }
-
-    complete_apps = ['cmsplugin_poll']
+    operations = [
+        migrations.CreateModel(
+            name='Choice',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('choice', models.CharField(max_length=200, verbose_name='choice')),
+                ('votes', models.IntegerField(default=0, verbose_name='votes')),
+            ],
+            options={
+                'verbose_name': 'Choice',
+                'verbose_name_plural': 'Choices',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Poll',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('question', models.CharField(max_length=300, verbose_name='question')),
+                ('pub_date', models.DateTimeField(verbose_name='date published')),
+                ('close_date', models.DateTimeField(null=True, verbose_name='date closed')),
+            ],
+            options={
+                'ordering': ('-pub_date',),
+                'db_table': 'cmsplugin_poll_poll',
+                'verbose_name': 'Poll',
+                'verbose_name_plural': 'Polls',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='PollPlugin',
+            fields=[
+                ('cmsplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin')),
+                ('poll', models.ForeignKey(verbose_name='Poll to display', to='cmsplugin_poll.Poll')),
+            ],
+            options={
+                'verbose_name': 'Poll plugin',
+                'verbose_name_plural': 'Poll plugins',
+            },
+            bases=('cms.cmsplugin',),
+        ),
+        migrations.AddField(
+            model_name='choice',
+            name='poll',
+            field=models.ForeignKey(verbose_name='poll', to='cmsplugin_poll.Poll'),
+            preserve_default=True,
+        ),
+    ]

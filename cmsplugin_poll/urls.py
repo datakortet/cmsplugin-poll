17ac876b3936aeecb1f1a7fa6cb5667c24bbade7
Erlend Dalen <ed@norsktest.no>
2020-09-16 13:26:01 +0200

    Fix urls (Dj 1.9).

--- cmsplugin_poll/urls.py
+++ cmsplugin_poll/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns
+from cmsplugin_poll import views
+from django.conf.urls import url
 
-urlpatterns = patterns(
-    'cmsplugin_poll.views',
-    (r'^$', 'index'),
-    (r'^(?P<poll_id>\d+)/$', 'detail'),
-    (r'^(?P<poll_id>\d+)/results/$', 'results'),
-    (r'^(?P<poll_id>\d+)/vote/$', 'vote'),
-)
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^(?P<poll_id>\d+)/$', views.detail),
+    url(r'^(?P<poll_id>\d+)/results/$', views.results),
+    url(r'^(?P<poll_id>\d+)/vote/$', views.vote),
+]

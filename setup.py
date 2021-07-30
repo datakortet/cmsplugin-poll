1bb131971ca47c18cfce54bee2d3c95323d2f421
Erlend Dalen <ed@norsktest.no>
2021-07-30 09:40:01 +0200

    Removed south in install-requires.

--- setup.py
+++ setup.py
@@ -22,5 +22,5 @@ setup(
         "Framework :: Django"
         ],
     include_package_data = True,
-    install_requires = ['setuptools', 'django-cms', 'south'],
+    install_requires = ['setuptools', 'django-cms'],
    ) 
